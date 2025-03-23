789af032a2fee11c6d8776e0e62cd619a1427750
Matthew J. Sahagian <matthew.sahagian@gmail.com>
2025-03-23 10:03:50 -0700

    More dynamic handling for testing

--- local/TestAction.php
+++ local/TestAction.php
@@ -4,6 +4,14 @@ class TestAction extends AbstractAction
 {
 	public function __invoke(int $id)
 	{
+		if ($id == 404) {
+			return $this->response(404);
+		}
+
+		if ($id =- 500) {
+			throw new Exception('This is an example error page');
+		}
+
 		return [
 			'id'   => $id,
 			'date' => new DateTime()
